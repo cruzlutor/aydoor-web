2817316663857f7fc0192b10a57a2d7cfcf245fe
lucho <cruz.torrez@gmail.com>
2015-07-05 10:12:08 -0500

    some fixes

--- js/modules/service/controllers/search.js
+++ js/modules/service/controllers/search.js
@@ -1,7 +1,6 @@
 aydoor.controller('SearchCtrl', ['$scope', '$location', 'AuthManager', 'Ads',  function($scope, $location, AuthManager, Ads) {
 
     $scope.filter = {};
-    $scope.serviceList = [1,2,3,4,5,6];
 
     $scope.init = function(){
         $scope.filter = $location.search();
@@ -21,19 +20,5 @@ aydoor.controller('SearchCtrl', ['$scope', '$location', 'AuthManager', 'Ads',  f
     }
 
     $scope.init();
-	// $scope.data = {};
-
-	// $scope.login = function(response){
- //        AuthManager.login(response.token);
-	// 	// Auth.save($scope.data, $scope.onLogin, $scope.onLoginErr );
-	// }
-
-	// $scope.onLogin = function(response){
-	// 	console.log(response);
-	// }
-
-	// $scope.onLoginErr = function(response){
-	// 	console.log(response);
-	// }
 
 }]);
